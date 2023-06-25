aa9339cc45ce25681b1c3c7f4053c5cc754df4f5
Jeremie Tavares <84427506+JeremieTavares@users.noreply.github.com>
2023-06-25 18:44:03 -0400

    js regex on user input

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,40 +1,48 @@
-import {InteractWithCalculator} from "./Class/IntereractCalculator.js";
+import { InteractWithCalculator } from "./Class/IntereractCalculator.js";
 let calculator = new InteractWithCalculator();
 
 const displayValue = () => {
-  const genderChoice = document.querySelectorAll("input[type='radio']");
-  const heightChoice = document.querySelector("input[type='text']");
-  genderChoice.forEach(radio => {
+    const genderChoice = document.querySelectorAll("input[type='radio']");
+    const heightChoice = document.querySelector("input[type='text']");
+    const divError = document.querySelector("#divError");
+    genderChoice.forEach(radio => {
 
-    radio.addEventListener('click', (e) => {
+        radio.addEventListener('click', (e) => {
 
-      switch (e.target.id) {
-        case 'homme':
-          genderChoice[1].parentElement.classList.add('unchecked')
-          genderChoice[0].parentElement.classList.remove('unchecked')
-          calculator.setGender('H');
-          break;
-        case 'femme':
-          genderChoice[0].parentElement.classList.add('unchecked')
-          genderChoice[1].parentElement.classList.remove('unchecked')
-          calculator.setGender('F');
-          break;
-      }
+            switch (e.target.id) {
+                case 'homme':
+                    genderChoice[1].parentElement.classList.add('unchecked')
+                    genderChoice[0].parentElement.classList.remove('unchecked')
+                    calculator.setGender('H');
+                    break;
+                case 'femme':
+                    genderChoice[0].parentElement.classList.add('unchecked')
+                    genderChoice[1].parentElement.classList.remove('unchecked')
+                    calculator.setGender('F');
+                    break;
+            }
 
-      if (heightChoice.value != '') {
-        console.log("allo");
-        calculator.init();
-      }
+            if (heightChoice.value != '' && heightChoice.value.match(/^[0-9]*\.?[0-9]*$/)) {
+                calculator.init();
+            }
+            
+        })
     })
-  })
 
-  heightChoice.addEventListener('input', () => {
-    calculator.setHeight(heightChoice.value);
+    heightChoice.addEventListener('input', () => {
 
-    if (genderChoice[0].checked || genderChoice[1].checked) {
-      calculator.init();
-    }
-  })
+        if (!heightChoice.value.match(/^[0-9]*\.?[0-9]*$/)) {
+            heightChoice.classList.add("is-invalid");
+        } else {
+            heightChoice.classList.remove("is-invalid");
+        }
+
+        calculator.setHeight(heightChoice.value);
+
+        if ((genderChoice[0].checked || genderChoice[1].checked) && heightChoice.value.match(/^[0-9]*\.?[0-9]*$/)) {
+            calculator.init();
+        }
+    })
 }
 
 displayValue();
